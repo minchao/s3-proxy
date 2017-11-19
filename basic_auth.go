4004f5a7a5c6ba68b940726e00e59f60810da25f
minchao <minchao.220@gmail.com>
2017-11-20 07:26:12 +0800

    Add GithubOrgAuth

--- basic_auth.go
+++ basic_auth.go
@@ -1,5 +1,11 @@
 package s3proxy
 
+import (
+	"context"
+
+	"github.com/google/go-github/github"
+)
+
 type BasicAuth interface {
 	authenticate(username, password string) bool
 }
@@ -12,6 +18,30 @@ type SimpleBasicAuth struct {
 func (ba *SimpleBasicAuth) authenticate(username, password string) bool {
 	if username == ba.Username && password == ba.Password {
 		return true
+	}
+	return false
+}
+
+type GithubOrgAuth struct {
+	LoginOrg string
+}
+
+func (ga *GithubOrgAuth) authenticate(username, password string) bool {
+	tp := github.BasicAuthTransport{
+		Username: username,
+		Password: password,
+	}
+	client := github.NewClient(tp.Client())
+
+	orgs, _, err := client.Organizations.List(context.Background(), "", nil)
+	if err != nil {
+		return false
+	}
+
+	for _, org := range orgs {
+		if org.Login != nil && ga.LoginOrg == *org.Login {
+			return true
+		}
 	}
 	return false
 }
